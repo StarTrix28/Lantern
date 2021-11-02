f0fb1f773467bc7296f1e9098dc2d74cc304e647
StarTrix <radim-hudec@seznam.cz>
2021-11-02 07:27:43 +0100

    reoriented to only python

--- src/main.py
+++ src/main.py
@@ -19,12 +19,18 @@ def open_file():
         editor.delete('1.0', END)
         editor.insert('1.0', code)
         set_file_path(path)
-def save_as():
+def save():
     global filetypes
     if file_path == '':
         path = asksaveasfilename(filetypes=filetypes)
     else:
         path = file_path
+    with open(path, 'w') as file:
+        code = editor.get('1.0', END)
+        file.write(code)
+        set_file_path(path)
+def save_as():
+    path = asksaveasfilename(filetypes=filetypes)
     with open(path, 'w') as file:
         code = editor.get('1.0', END)
         file.write(code)
@@ -36,31 +42,19 @@ def run():
         path = asksaveasfilename(filetypes=filetypes)
         set_file_path(path)
         return
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    process = subprocess.Popen("python", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
     code_output.insert('1.0', output)
     code_output.insert('1.0', error)
-def Build_as(a):
-    global filetypes
-    global command
-    if not(file_path == ''):
-        if a == 'java':
-            command = f'java'
-            return
-        if a == 'python':
-            command = f'python'
-            return
-        return
-    elif file_path == '':
-        path = asksaveasfilename(filetypes=filetypes)
-        set_file_path(path)
-        return
+
+
+
 def type(word):
     keyboard.write(word)
 
 
 class Helper:
-    # qwerty
+    # qwertz
 
     keyboard.add_hotkey('SHIFT+)', lambda: type(")"), timeout=0)
     keyboard.add_hotkey('CTRL+ALT+f', lambda: type("]"), timeout=0)
@@ -73,8 +67,7 @@ class Helper:
 class ShortCuts:
     class main:
         keyboard.add_hotkey('CTRL+b', lambda: run(), timeout=0)
-        keyboard.add_hotkey('CTRL+s', lambda: type("]"), timeout=0)
-    class python:
+        keyboard.add_hotkey('CTRL+s', lambda: save_as(), timeout=0)
         keyboard.add_hotkey('CTRL+ALT+c', lambda: type("class classname:"), timeout=0)
         keyboard.add_hotkey('CTRL+ALT+d', lambda: type("def defname:"), timeout=0)
 
@@ -85,6 +78,7 @@ def java_class_create():
 
 }''')
 """
+
 
 menu_bar = Menu(compiler)
 
@@ -98,14 +92,14 @@ class menu:
         file_menu.add_command(label='Save As', command=save_as)
         file_menu.add_command(label='Exit', command=exit)
         menu_bar.add_cascade(label='File', menu=file_menu)
+    class Python:
+        global menu_bar
+        python_bar =Menu(menu_bar, tearoff=0)
+        python_bar.add_command(label='class', command=type("class classname:"))
     class BuildMenu:
         global menu_bar
         build_bar = Menu(menu_bar, tearoff=0)
         build_bar.add_command(label='Build', command=run)
-        buildas_subbar = Menu(build_bar, tearoff=0)
-        buildas_subbar.add_command(label='Python', comand=Build_as("python"))
-        buildas_subbar.add_command(label='Java', comand=Build_as("java"))
-        build_bar.add_cascade(label='Build As', menu=buildas_subbar)
         menu_bar.add_cascade(label='Build', menu=build_bar)
     class toolmenu:
         global menu_bar
